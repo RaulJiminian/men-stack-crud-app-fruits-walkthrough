d3b1237dc684eeea0fdbaee913c78cdca9b7747a
RaulJiminian <raul.e.jiminian@gmail.com>
2026-01-12 16:22:22 -0500

    post route setup

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const logger = require("morgan");
 const db = require("./db/connection.js");
+const Fruit = require("./models/fruit.js");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
 
 // Routes
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+});
+
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+
+  await Fruit.create(req.body);
+
+  res.redirect("/fruits/new");
 });
 
 db.on("connected", () => {
